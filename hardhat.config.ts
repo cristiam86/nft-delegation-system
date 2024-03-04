199f45ee9b0ba861e2b85088fda0ef0596a82840
Cristiam Da Silva <cristiam86@gmail.com>
2024-03-04 17:16:02 +0100

    feat: added deployment command

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const MUMBAI_PRIVATE_KEY = vars.get("MUMBAI_PRIVATE_KEY");
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [MUMBAI_PRIVATE_KEY]
+    },
+  },
 };
 
 export default config;
